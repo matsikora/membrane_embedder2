15c4406f57b226bc5235b75bdd8a4dc93ad16bc2
matsikora <matsikora@gmail.com>
2018-12-21 09:05:42 +0000

    created repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='membrane-embedder2',
+      version='0.1',
+      description='Create multiprotein systems for complexes',
+      url='http://github.com/matsikora/membrane-embedder2',
+      author='Mateusz Sikora',
+      author_email='matsikora@gmail.com',
+      license=' GNU GPLv3 ',
+      packages=['membrane_embedder2'],
+      zip_safe=False)
