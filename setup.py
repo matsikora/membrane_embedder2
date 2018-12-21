01de635876940a1e7112dddcd9fa1151ab8d849d
matsikora <matsikora@gmail.com>
2018-12-21 09:20:52 +0000

    readme

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(name='membrane-embedder2',
       version='0.1',
       description='Create multiprotein systems for complexes',
-      url='http://github.com/matsikora/membrane-embedder2',
+      url='https://github.com/matsikora/membrane_embedder2',
       author='Mateusz Sikora',
       author_email='matsikora@gmail.com',
       license=' GNU GPLv3 ',
       packages=['membrane_embedder2'],
+      install_requires=['numpy','mdanalysis'],
       zip_safe=False)
